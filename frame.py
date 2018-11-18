7be0cfb55356e243affc69b5546ffffc6f42b800
ZHIYONGCEHN <827142031@qq.com>
2018-11-18 21:02:23 +0800

    增加统计函数

--- frame.py
+++ frame.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 
 cols = ['A', 'B']
-df = pd.DataFrame(np.random.randn(20, 2), Columns = cols)
+df = pd.DataFrame(np.random.randn(20, 2), columns = cols)
 
-
+df_stat = df.describe()
 
 if __name__ == "__main__":
-    print(df)
+    print(df_stat)
     
     
